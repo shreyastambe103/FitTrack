f7750ca229dd182b21533f7025c5b4fff6040a6e
shreyastambe-dev <shreyastambe.dev@projecthumancity.com>
2024-11-03 11:00:39 +0530

    added auth hooks and controllers

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,4 +1,5 @@
-import { createContext, useReducer} from 'react'
+//frontend/src/context/AuthContext.js
+import { createContext, useReducer, useEffect} from 'react'
 
 export const AuthContext = createContext()
 
@@ -17,6 +18,14 @@ export const AuthContextProvider = ({ children }) => {
     const [state,dispatch] = useReducer(authReducer,{
         user: null
     })
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('user'))
+
+        if(user){
+            dispatch({type: 'LOGIN', payload: user})
+        }
+    },[])
+
     console.log('AuthContext state: ',state)
 
     return(
